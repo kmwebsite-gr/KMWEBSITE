8a204d5029c96faaafa64a1a8656f2e4b090747a
KMWEBSITE <97700993+kmwebsite-gr@users.noreply.github.com>
2026-09-18 19:23:48 +0300

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -103,6 +103,10 @@ app.get("/sitemap.xml", (req, res) => {
     `<?xml version="1.0" encoding="UTF-8"?>\n` +
       `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">` +
       `<url><loc>${baseUrl}/</loc><changefreq>monthly</changefreq><priority>1.0</priority></url>` +
+      `<url><loc>${baseUrl}/kouzines.html</loc><changefreq>monthly</changefreq><priority>0.8</priority></url>` +
+      `<url><loc>${baseUrl}/ntoulapes.html</loc><changefreq>monthly</changefreq><priority>0.8</priority></url>` +
+      `<url><loc>${baseUrl}/portes.html</loc><changefreq>monthly</changefreq><priority>0.8</priority></url>` +
+      `<url><loc>${baseUrl}/eidikes-kataskeves.html</loc><changefreq>monthly</changefreq><priority>0.8</priority></url>` +
       `</urlset>`
   );
 });
